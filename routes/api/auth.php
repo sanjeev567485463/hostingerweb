04e7b80a55ccbd6ddb6af235139e55f5cbcad574
sanjeev kumar <nk973711@gmail.com>
2026-04-05 19:20:01 +0530

    fix: remove api social auth routes

--- routes/api/auth.php
+++ routes/api/auth.php
@@ -11,15 +11,9 @@ Route::group(['namespace' => 'Auth', 'middleware' => ['api.request.type']], func
     Route::post('/forget-password', ['as' => 'forgot', 'uses' => 'ForgotPasswordController@forgot']);
     Route::post('/reset-password/{token}', ['as' => 'updatePassword', 'uses' => 'ResetPasswordController@updatePassword']);
     Route::post('/verification', ['as' => 'verification', 'uses' => 'VerificationController@confirmCode']);
-    Route::get('/google', ['as' => 'google', 'uses' => 'SocialiteController@redirectToGoogle']);
-    Route::get('/facebook', ['as' => 'google', 'uses' => 'SocialiteController@redirectToFacebook']);
-    Route::post('/google/callback', ['as' => 'google_callback', 'uses' => 'SocialiteController@handleGoogleCallback']);
-    Route::post('/facebook/callback', ['as' => 'facebook_callback', 'uses' => 'SocialiteController@handleFacebookCallback']);
-
    // Route::get('/reff/{code}', 'ReferralController@referral');
 
 });
 
 Route::post('/logout', ['as' => 'logout', 'uses' => 'Auth\LoginController@logout', 'middleware' => ['api.auth']]);
-
 
